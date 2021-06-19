08ad6809c8ef14a16ab00d675a5735f6b832ff86
SiyathandanaNontolwana <1462572@students.wits.ac.za>
2021-06-19 18:56:52 +0200

    Player Movement done

---
+++ June Exam Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D playerRB;
+    public float playerSpeed = 5f;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        playerRB.MovePosition(playerRB.position + movement * playerSpeed * Time.fixedDeltaTime);
+       
+    }
+}
