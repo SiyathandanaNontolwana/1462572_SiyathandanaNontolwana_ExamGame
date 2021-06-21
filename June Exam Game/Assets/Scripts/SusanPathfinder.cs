8e44677a3ba7be745de5b91036d753a2a1978022
SiyathandanaNontolwana <1462572@students.wits.ac.za>
2021-06-21 14:30:10 +0200

    Susan AI update
    She can now randomly choose which location to go to

--- June Exam Game/Assets/Scripts/SusanPathfinder.cs
+++ June Exam Game/Assets/Scripts/SusanPathfinder.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class SusanPathfinder : MonoBehaviour
 {
+    //Variables
+    public float susanSpeed = 4.5f;
+    public int randomLocation;
+
+    //Total target locations
+    public Transform [] totalTargetLocations;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomLocation = Random.Range(0, totalTargetLocations.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+      
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, totalTargetLocations[randomLocation].position, susanSpeed * Time.fixedDeltaTime);
+    }
+
+    private void RayLocationChecker()
+    {
+
+    }
+
+    public void LocationStorer()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawLine(rayCastOrigin.position, totalTargetLocations[1].position);
+        //Gizmos.DrawLine(rayCastOrigin.position, totalTargetLocations[0].position);
     }
 }
